83a536d1075d56159c74fea8b40e90af784b2187
Leonardo Braz <lhleonardo@hotmail.com>
2016-10-07 23:40:17 -0400

    Adicionando usuário padrão caso não exista nenhum cadastrado na base

--- app/server/bootstrap.js
+++ app/server/bootstrap.js
@@ -1,2 +1,14 @@
 Meteor.startup(function () {
+  if (Meteor.users.find().count() == 0) {
+    var user = {
+      username: "lhleonardo",
+      email: "lhleonardo@hotmail.com",
+      password: "leonardo"
+    };
+
+    Accounts.createUser(user);
+
+    console.log('user criado');
+  }
+
 });
