68b438daa3bf2b00faefb126883f38045876c9a8
Leonardo Braz <lhleonardo@hotmail.com>
2016-10-09 20:57:58 -0400

    Criando a listagem de fieis com o tabular

--- app/lib/tables/tables.js
+++ app/lib/tables/tables.js
@@ -3,6 +3,8 @@ TabularTables = {};
 TabularTables.Fieis = new Tabular.Table({
   name: "Fieis",
   collection: Fieis,
+  responsive: true,
+  autoWidth: false,
   columns: [
     {data: "nome", title: "Nome"},
     {data: "tipo", title: "Tipo"},
