d166ffee94885a8e6aec4a3b89b0aa21ea2b96d5
Leonardo Braz <lhleonardo05@gmail.com>
2016-10-19 15:09:00 -0400

    Criando o controlador de rota para os usuários

---
+++ app/lib/controllers/usuarios_controller.js
@@ -0,0 +1,76 @@
+
+UsersController = RouteController.extend({
+
+  // A place to put your subscriptions
+  // this.subscribe('items');
+  // // add the subscription to the waitlist
+  // this.subscribe('item', this.params._id).wait();
+
+  subscriptions: function() {
+    if (Roles.userIsInRole(Meteor.user(), ['admin'], Roles.GLOBAL_GROUP)) {
+      this.subscribe('organizacoes');
+    }
+  },
+
+  create: function() {
+    this.render('CreateUser', {});
+  },
+
+  edit: function() {
+    this.render('UpdateUser', {});
+  },
+
+  list: function() {
+    this.render('ListUser', {});
+  },
+
+  view: function () {
+    this.render('ViewUser', {});
+  },
+
+  // A data function that can be used to automatically set the data context for
+  // our layout. This function can also be used by hooks and plugins. For
+  // example, the "dataNotFound" plugin calls this function to see if it
+  // returns a null value, and if so, renders the not found template.
+  // return Posts.findOne({_id: this.params._id});
+
+  data: function () {
+    return Meteor.users().find({_id : this.params._id});
+  },
+
+  // You can provide any of the hook options
+
+  onRun: function () {
+    this.next();
+  },
+  onRerun: function () {
+    this.next();
+  },
+  onBeforeAction: function () {
+    if (!Meteor.userId()) {
+      this.redirect('login');
+      this.stop();
+    }
+
+    if (Roles.userIsInRole(Meteor.user(), ['admin'], Roles.GLOBAL_GROUP) == false) {
+      this.render('Unauthorized');
+    }
+    this.next();
+  },
+
+  // The same thing as providing a function as the second parameter. You can
+  // also provide a string action name here which will be looked up on a Controller
+  // when the route runs. More on Controllers later. Note, the action function
+  // is optional. By default a route will render its template, layout and
+  // regions automatically.
+  // Example:
+  //  action: 'myActionFunction'
+
+  action: function () {
+    this.render();
+  },
+  onAfterAction: function () {
+  },
+  onStop: function () {
+  }
+});
