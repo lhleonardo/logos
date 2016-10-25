c369e740f1355fb1f3bd044c442885e62e75602d
Leonardo Braz <lhleonardo@hotmail.com>
2016-10-25 00:00:12 -0400

    estruturando o template das pastorais

---
+++ app/client/templates/pastorais/create_pastoral/create_pastoral.js
@@ -0,0 +1,23 @@
+/*****************************************************************************/
+/* CreatePastoral: Event Handlers */
+/*****************************************************************************/
+Template.CreatePastoral.events({
+});
+
+/*****************************************************************************/
+/* CreatePastoral: Helpers */
+/*****************************************************************************/
+Template.CreatePastoral.helpers({
+});
+
+/*****************************************************************************/
+/* CreatePastoral: Lifecycle Hooks */
+/*****************************************************************************/
+Template.CreatePastoral.onCreated(function () {
+});
+
+Template.CreatePastoral.onRendered(function () {
+});
+
+Template.CreatePastoral.onDestroyed(function () {
+});
---
+++ app/client/templates/pastorais/update_pastoral/update_pastoral.js
@@ -0,0 +1,23 @@
+/*****************************************************************************/
+/* UpdatePastoral: Event Handlers */
+/*****************************************************************************/
+Template.UpdatePastoral.events({
+});
+
+/*****************************************************************************/
+/* UpdatePastoral: Helpers */
+/*****************************************************************************/
+Template.UpdatePastoral.helpers({
+});
+
+/*****************************************************************************/
+/* UpdatePastoral: Lifecycle Hooks */
+/*****************************************************************************/
+Template.UpdatePastoral.onCreated(function () {
+});
+
+Template.UpdatePastoral.onRendered(function () {
+});
+
+Template.UpdatePastoral.onDestroyed(function () {
+});
